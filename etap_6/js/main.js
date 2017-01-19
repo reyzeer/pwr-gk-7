7147100410e32bd313e49076f5c66ea828b41641
reyzeer <czarneckizbigniew0@gmail.com>
2017-01-20 00:37:12 +0100

    Etap 6 - fixed

--- etap_6/js/main.js
+++ etap_6/js/main.js
@@ -4,6 +4,15 @@ var gl_ctx;
 
 var _position;
 var _color;
+var _PosMatrix;
+var _MovMatrix;
+var _ViewMatrix;
+var _matrixProjection;
+var _matrixMovement;
+var _matrixView;
+
+var rotationSpeed = 0.001;
+var zoomRatio = -6;
 
 var _triangleVertexBuffer;
 var _triangleFacesBuffer;
@@ -16,6 +25,7 @@ function runWebGL() {
     gl_ctx = gl_getContext(gl_canvas);
     gl_initShaders();
     gl_initBuffers();
+    gl_setMatrix();
     gl_draw();
 }
 
@@ -37,14 +47,19 @@ function gl_getContext(canvas) {
     return ctx;
 }
 
-// shadery
+/**
+ * shadery
+ */
 function gl_initShaders () {
     var vertexShader = "\n\
-        attribute vec2 position;\n\
+        attribute vec3 position;\n\
+        uniform mat4 PosMatrix;\n\
+        uniform mat4 MovMatrix;\n\
+        uniform mat4 ViewMatrix; \n\
         attribute vec3 color;\n\
         varying vec3 vColor;\n\
         void main(void) {\n\
-            gl_Position = vec4(position, 0., 1.);\n\
+            gl_Position = PosMatrix * ViewMatrix * MovMatrix * vec4(position, 1.);\n\
             vColor = color;\n\
         }";
 
@@ -75,22 +90,30 @@ function gl_initShaders () {
     gl_ctx.attachShader(shaderProgram, shaderFragment);
 
     gl_ctx.linkProgram(shaderProgram);
+
+    _PosMatrix = gl_ctx.getUniformLocation(shaderProgram, "PosMatrix");
+    _MovMatrix = gl_ctx.getUniformLocation(shaderProgram, "MovMatrix");
+    _ViewMatrix = gl_ctx.getUniformLocation(shaderProgram, "ViewMatrix");
+
     _position = gl_ctx.getAttribLocation(shaderProgram, "position");
     _color = gl_ctx.getAttribLocation(shaderProgram, "color");
+
     gl_ctx.enableVertexAttribArray(_position);
     gl_ctx.enableVertexAttribArray(_color)
     gl_ctx.useProgram(shaderProgram);
 }
 
-// bufory
+/**
+ * bufory
+ */
 function gl_initBuffers () {
     var triangleVertices = [
-        -1, -1,
-        0, 0, 1,
-        1, -1,
-        1, 1, 1,
-        1, 1,
-        1, 0, 0
+        -1, -1, 0, // bottom left
+        0, 0, 1, // submit color: blue
+        1, -1, 0, // bottom right
+        1, 1, 1, // submit color: white
+        1, 1, 0, // top right
+        1, 0, 0 // submit color: red
     ];
 
     _triangleVertexBuffer = gl_ctx.createBuffer();
@@ -112,22 +135,57 @@ function gl_initBuffers () {
     );
 }
 
-// rysowanie
+/**
+ * Macierz
+ */
+function gl_setMatrix () {
+    _matrixProjection = MATRIX.getProjection(
+        40,
+        gl_canvas.width/gl_canvas.height,
+        1,
+        100
+    );
+    _matrixMovement = MATRIX.getIdentityMatrix();
+    _matrixView = MATRIX.getIdentityMatrix();
+    MATRIX.translateZ(_matrixView, zoomRatio);
+}
+
+/**
+ * rysowanie
+ */
 function gl_draw() {
     gl_ctx.clearColor(0.0, 0.0, 0.0, 0.0);
+    gl_ctx.enable(gl_ctx.DEPTH_TEST);
+    gl_ctx.depthFunc(gl_ctx.LEQUAL);
+    gl_ctx.clearDepth(1.0);
 
-    var animate = function () {
+    var timeOld = 0;
+    var animate = function (time) {
+        var dAngle = rotationSpeed * (time - timeOld);
+        MATRIX.rotateY(_matrixMovement, dAngle);
+        timeOld = time;
+
         gl_ctx.viewport(0.0, 0.0, gl_canvas.width, gl_canvas.height);
-        gl_ctx.clear(gl_ctx.COLOR_BUFFER_BIT);
+        gl_ctx.clear(gl_ctx.COLOR_BUFFER_BIT | gl_ctx.DEPTH_BUFFER_BIT);
 
-        gl_ctx.vertexAttribPointer(_position, 2, gl_ctx.FLOAT, false, 4*(2+3), 0);
-        gl_ctx.vertexAttribPointer(_color, 3, gl_ctx.FLOAT, false, 4*(2+3), 2*4);
+        gl_ctx.uniformMatrix4fv(_PosMatrix, false, _matrixProjection);
+        gl_ctx.uniformMatrix4fv(_MovMatrix, false, _matrixMovement);
+        gl_ctx.uniformMatrix4fv(_ViewMatrix, false, _matrixView);
+
+        gl_ctx.vertexAttribPointer(_position, 3, gl_ctx.FLOAT, false, 4*(3+3), 0);
+        gl_ctx.vertexAttribPointer(_color, 3, gl_ctx.FLOAT, false, 4*(3+3), 3*4);
+
+        // Błąd w instrukcji!!!
+        // gl_ctx.vertexAttribPointer(_position, 2, gl_ctx.FLOAT, false, 4*(2+3), 0);
+        // gl_ctx.vertexAttribPointer(_color, 3, gl_ctx.FLOAT, false, 4*(2+3), 2*4);
 
         gl_ctx.bindBuffer(gl_ctx.ARRAY_BUFFER, _triangleVertexBuffer);
         gl_ctx.bindBuffer(gl_ctx.ELEMENT_ARRAY_BUFFER, _triangleFacesBuffer);
 
         gl_ctx.drawElements(gl_ctx.TRIANGLES, 3, gl_ctx.UNSIGNED_SHORT, 0);
         gl_ctx.flush();
+
+        window.requestAnimationFrame(animate);
     };
-    animate();
+    animate(0);
 }
